69c03d108d767c0e825192b7e479aca175f09c2c
Bart Reardon <bart.reardon@csiro.au>
2019-11-01 17:03:16 +1100

    Update download_o365_apps.sh
    Check headers and use the actual package name
    initial support for other packages (Yammer - Todo, see how generic we can make this)
    initial support for install from DMG - simple mount and copy *.app to /Applications.

--- download_o365_apps.sh
+++ download_o365_apps.sh
@@ -12,7 +12,12 @@ O365Latest="https://macadmins.software/latest.xml"
 
 APPName=$4
 
-PKGID="com.microsoft.$(echo ${APPName} | /usr/bin/awk '{print tolower($0)}').standalone.365"
+# don't like this - work out a way to grab the pkgID some other way
+if [[ ${APPName} == "Yammer" ]]; then
+    PKGID="com.microsoft.$(echo ${APPName} | /usr/bin/awk '{print tolower($0)}').standalone"
+else
+    PKGID="com.microsoft.$(echo ${APPName} | /usr/bin/awk '{print tolower($0)}').standalone.365"
+fi
 
 XML=$(/usr/bin/curl -s ${O365Latest})
 
@@ -21,7 +26,12 @@ if [[ ! -z ${XML} ]]; then
     O365Version=$(/usr/bin/xmllint --xpath '//latest/o365/text()' - <<< "${XML}")
 
     PKGURL=$(/usr/bin/xmllint --xpath '//latest/package[id="'"${PKGID}"'"]/download/text()' - <<< "${XML}")
-    TMPFileName="/tmp/MS${APPName}_${O365Version}.pkg"
+    
+    # get the header and package name from the URL
+    PKGName=$(curl -sIL ${PKGURL} | grep -i location | awk -F "/" '{print $NF}' | tr -d '\r')
+    PKGEXT=$(echo "${PKGName}" | awk -F "." '{print $NF}' | tr -d '\r')
+    
+    TMPFileName="/tmp/${PKGName}"
 
     echo "${APPName} pkgurl ${PKGURL}"
 
@@ -44,14 +54,30 @@ if [[ ! -z ${XML} ]]; then
     /bin/sleep 2
     echo "Command: DeterminateManualStep: 1" >> ${DEPNotifyLog}
     
-    # run instaler in verboseR mode to give installer percentage
-    # when we find a % sign, capture up until the decimal point and then output
-    # we don't capture the full percentage as installer outputs to 6 decimal places which is a tad overkill
-    /usr/sbin/installer -pkg ${TMPFileName} -target / -verboseR 2>&1 | while read -r -n1 char; do
-        [[ $char == % ]] && keep=1 ;
-        [[ $char =~ [0-9] ]] && [[ $keep == 1 ]] && progress="$progress$char" ;
-        [[ $char == . ]] && [[ $keep == 1 ]] && echo "Status: Installing Microsoft ${APPName} ${progress}%" >> ${DEPNotifyLog} && progress="" && keep=0 ;
-    done
+    # see if we're installing from PKG or DMG
+    if [[ ${PKGEXT} == "pkg" ]]; then
+	    # run instaler in verboseR mode to give installer percentage
+	    # when we find a % sign, capture up until the decimal point and then output
+	    # we don't capture the full percentage as installer outputs to 6 decimal places which is a tad overkill
+	    /usr/sbin/installer -pkg ${TMPFileName} -target / -verboseR 2>&1 | while read -r -n1 char; do
+		[[ $char == % ]] && keep=1 ;
+		[[ $char =~ [0-9] ]] && [[ $keep == 1 ]] && progress="$progress$char" ;
+		[[ $char == . ]] && [[ $keep == 1 ]] && echo "Status: Installing Microsoft ${APPName} ${progress}%" >> ${DEPNotifyLog} && progress="" && keep=0 ;
+	    done
+    elif [[ ${PKGEXT} == "dmg" ]]; then
+        echo "Status: Mounting ${PKGName}" >> ${DEPNotifyLog}
+        mountPath=$(hdiutil attach ${TMPFileName} | grep "/Volumes" | awk '{print $NF}')
+        echo "Status: Mounted at ${mountPath}" >> ${DEPNotifyLog}
+        # copy all .app to /Volumes
+        for appBundle in $(ls -d ${mountPath}/*.app); do
+            echo "Status: Copying ${appBundle} to Applications" >> ${DEPNotifyLog}
+            cp -r ${appBundle} /Applications/
+        done
+        echo "Status: Ejecting ${mountPath}" >> ${DEPNotifyLog}
+        hdiutil eject ${mountPath}
+    else
+        echo "Status: There was an error determiningn package type for ${PKGName}" >> ${DEPNotifyLog}
+    fi
     
     echo "Command: DeterminateManualStep: 1" >> ${DEPNotifyLog}
     echo "Status: Microsoft ${APPName} Install Complete" >> ${DEPNotifyLog}
@@ -63,6 +89,6 @@ if [[ ! -z ${XML} ]]; then
     # set determinate to back to auto
     echo "Command: Determinate: ${DEPSteps}" >> ${DEPNotifyLog}
 else
-	echo "Status: Error Downloading Microsoft ${APPName} ${O365Version} from ${O365Latest}..." >> ${DEPNotifyLog}
+    echo "Status: Error Downloading Microsoft ${APPName} ${O365Version} from ${O365Latest}..." >> ${DEPNotifyLog}
     /bin/sleep 2
 fi
